b3a41cccd77384b4acd5517dd5d1a361fa9e5aa6
doh <theshawns0n@gmail.com>
2021-01-01 14:02:33 -0500

    section 2

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,20 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
+
         boolean hasPerfectAttendance = true;
+        String firstName = "Kayla";
+        String lastName = "Hammond";
+        String fullName = firstName + " " + lastName;
+        char studentFirstInitial = firstName.charAt(0);
+        char studentLastInitial = lastName.charAt(0);
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(fullName);
+
     }
 }
